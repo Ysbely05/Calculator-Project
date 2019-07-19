96fad5b1d0c5f12778c074a6e2988f5cc3b28c0e
EC2 Default User <ec2-user@ip-172-31-34-223.us-east-2.compute.internal>
2019-07-19 13:21:32 +0000

    Calculator Project

---
+++ interest.py
@@ -0,0 +1,10 @@
+fav_movie =[ 
+    {"Name":"Akshay", "Movie": "Star Wars"},
+    {"Name":"Crystal", "Movie": "Orphan" },
+    {"Name":"Vanessa", "Movie": "Home" },
+    {"Name":"Sophia", "Movie": "Legally Blonde"},
+    {"Name":"Eric", "Movie": "Pulp Fiction" },
+    {"Name":"Marc", "Movie": "XMocker"},
+    {"Name":"Jose", "Movie": "Narnia" }
+      ]
+print(fav_movie[3])
